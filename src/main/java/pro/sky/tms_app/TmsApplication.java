6e78b0fb625842423889258f1363c8e6be6d6f53
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2025-03-15 00:07:42 +0300

    Task Management System - начальный коммит

---
+++ src/main/java/pro/sky/tms_app/TmsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.tms_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/tms_app/TmsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.tms_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
